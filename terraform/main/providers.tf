ca03dddf3b6f73fe43effeaba31ca9ad32b266a0
“Sunoj <l00162972@student.lyit.ie>
2022-05-11 17:50:23 +0100

    Update providers.tf
    delete  provider google

--- terraform/main/providers.tf
+++ terraform/main/providers.tf
@@ -1,11 +1,6 @@
 terraform {
   required_providers {
 
-    google = {
-      source  = "hashicorp/google"
-      version = "~> 4.0"
-    }
-    
     kubernetes = {
       source  = "hashicorp/kubernetes"
       version = ">= 1.13.0"
